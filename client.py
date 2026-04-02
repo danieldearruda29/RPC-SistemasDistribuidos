52091fc9fd5dcda35a82d2c1e147c34ed3523c08
gabrielDMBN <gabrieldahermonteiro@gmail.com>
2026-04-02 20:14:19 -0300

    questao 2 (teste)

--- client.py
+++ client.py
@@ -1,25 +1,13 @@
 import rpyc
 import sys
-import time
 
 if len(sys.argv) < 2:
     exit("Usage {} IP_DO_SERVIDOR".format(sys.argv[0]))
 
 server = sys.argv[1]
-conn = rpyc.connect(server,18861)
 
-# input do usuário
-n = int(input("Digite um número para o tamanho de um vetor de elementos sequenciais: "))
+conn = rpyc.connect(server, port=18861)
 
-# cria o vetor de 0 até n-1
-vetor = list(range(n))
-
-#mostra vetor criado aleatoriamente
-print("Vetor criado: ", vetor)
-
-# mede o tempo de receber de volta a soma do vetor utilizando o método do servidor
-start = time.time()
-print(conn.root.sum_vector(vetor))
-
-end = time.time()
-print("Tempo Client:", end-start)
+print(conn.root)
+print(conn.root.get_answer())
+print(conn.root.the_real_answer_though)
--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 import rpyc
-import time
+
 class MyService(rpyc.Service):
     def on_connect(self, conn):
-    # código que é executado quando uma conexão é iniciada, caso seja necessário
-        pass
+        print("Cliente conectado")
+
     def on_disconnect(self, conn):
-    # código que é executado quando uma conexão é finalizada, caso seja necessário
-        pass
-    def exposed_get_answer(self): # este é um método exposto
+        print("Cliente desconectado")
+
+    def exposed_get_answer(self):
         return 42
-    exposed_the_real_answer_though = 43 # este é um atributo exposto
-    def get_question(self): # este método não é exposto
+
+    exposed_the_real_answer_though = 43
+
+    def get_question(self):
         return "Qual é a cor do cavalo branco de Napoleão?"
 
-    #soma vetor do cliente
-    def exposed_sum_vector(self, vetor):
-        start = time.time()
-        num = sum(vetor)
-        end = time.time()
-        print("Tempo Server:", end - start)
-
-        return num
-
-#Para iniciar o servidor
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
 
